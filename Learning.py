4d8ba935c85aa55260aaa94e106fa5b8a78396e4
LordGudzo <lordgudzo@ukr.net>
2023-12-24 14:47:45 +0300

    uses bs4 and create fakeUserAgent

--- Learning.py
+++ Learning.py
@@ -1,9 +1,22 @@
-# pip install requests
-# pip install bs4
+# pip install requests    - for requests))
+# pip install bs4         - for parsing
+# pip install fake-useragent   - for changes user-agent
+
 import requests
+from bs4 import BeautifulSoup
+import fake_useragent
 
-link = "https://it-black.ru/tpost/r04gg7ugn1-chisla-v-python"
-responce = requests.get(link).text           #.content - get information in bytes
+user = fake_useragent.UserAgent().random
+header = { 'User-Agent': user}
 
-with open("1.html", "w", encoding="utf-8") as file:  #save responce as html file
-    file.write(responce)
+
+link = "https://wtools.io/ru/check-my-user-agent"
+responce = requests.get(link, headers = header).text           #.content - get information in bytes
+
+#   with open("1.html", "w", encoding="utf-8") as file:  #save responce as html file
+#      file.write(responce)
+
+soup = BeautifulSoup(responce, "lxml")
+user_Agent = soup.find('pre', id = "resultInputpre").text
+
+print(f"You user Agent: {user_Agent}")
